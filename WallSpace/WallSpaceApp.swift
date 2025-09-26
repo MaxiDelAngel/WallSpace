3b172520b88d19c71612b679a2613a3814520f26
roberto molina <jose.molina@iest.edu.mx>
2025-09-26 17:44:56 -0600

    Inicializamos Repositorio

---
+++ WallSpace/WallSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallSpaceApp.swift
+//  WallSpace
+//
+//  Created by WIN603 on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WallSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
